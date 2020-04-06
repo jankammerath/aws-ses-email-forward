109a06a19e70604c8ab54c894c5fb89837008e29
Jan Kammerath <jankammerath@googlemail.com>
2020-04-06 20:01:38 +0200

    first template version

---
+++ Lambda/app.js
@@ -0,0 +1,12 @@
+/**
+ * This lambda function receives notifications from
+ * SES about new emails and forwards them to the 
+ * new destination email address as defined in the
+ * environment variables
+ */
+exports.lambdaHandler = async (event, context) => {
+
+
+    /* finish the script */
+    return true;
+}
